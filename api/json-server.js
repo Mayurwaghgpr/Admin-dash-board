f71132519dd476f66940d64af73df8ca118040aa
Mayur Wagh <mayur2002wagh@gmail.com>
2024-12-08 20:23:18 +0530

    Set up JSON Server with serverless function

---
+++ api/json-server.js
@@ -0,0 +1,16 @@
+import { createServer } from 'json-server';
+import { join } from 'path';
+
+export default (req, res) => {
+  const server = createServer();
+  const router = server.router(join(process.cwd(), 'db.json'));
+  const middlewares = jsonServer.defaults();
+
+  server.use(middlewares);
+  server.use(router);
+
+  // Handling the request to the function
+  server.listen(3000, () => {
+    res.status(200).json({ message: "Server running" });
+  });
+};
